086bd07c568870907f7154285509ccbe44c2264b
Bhanu3791 <bb7207@srmist.edu.in>
2026-02-24 15:23:56 +0530

    UC5 - Render OOPS banner using inline array initialization

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,18 +2,16 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        // Create array to store banner lines
-        String[] lines = new String[7];
+        String[] lines = {
+            String.join(" ", " *** ", " *** ", " ***** ", " ***** "),
+            String.join(" ", "*   *", "*   *", "*   * ", "*     "),
+            String.join(" ", "*   *", "*   *", "*   * ", "*     "),
+            String.join(" ", "*   *", "*   *", "***** ", "***** "),
+            String.join(" ", "*   *", "*   *", "*     ", "    * "),
+            String.join(" ", "*   *", "*   *", "*     ", "    * "),
+            String.join(" ", " *** ", " *** ", "*     ", "***** ")
+        };
 
-        lines[0] = String.join(" ", " *** ", " *** ", " ***** ", " ***** ");
-        lines[1] = String.join(" ", "*   *", "*   *", "*      ", "*     ");
-        lines[2] = String.join(" ", "*   *", "*   *", "*****  ", " **** ");
-        lines[3] = String.join(" ", "*   *", "*   *", "*      ", "     *");
-        lines[4] = String.join(" ", "*   *", "*   *", "*      ", "     *");
-        lines[5] = String.join(" ", "*   *", "*   *", "*      ", "*     ");
-        lines[6] = String.join(" ", " *** ", " *** ", " ***** ", " ***** ");
-
-        // Print using for-each loop
         for (String line : lines) {
             System.out.println(line);
         }

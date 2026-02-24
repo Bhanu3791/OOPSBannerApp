7ffb0da75a28c842c40bc09ce8405ee056739d87
Bhanu3791 <bb7207@srmist.edu.in>
2026-02-24 15:33:45 +0530

    UC6 implemented using static helper methods

--- OOPSBannerApp/OOPSBannerApp.java
+++ OOPSBannerApp/OOPSBannerApp.java
@@ -1,7 +1,54 @@
 public class OOPSBannerApp {
 
-    public static void main(String[] args) {
-        System.out.println("OOPS");
+    // Method to generate pattern for letter 'O'
+    public static String[] getOPattern() {
+        return new String[]{
+            " *** ",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*   *",
+            " *** "
+        };
     }
 
+    // Method to generate pattern for letter 'P'
+    public static String[] getPPattern() {
+        return new String[]{
+            "**** ",
+            "*   *",
+            "*   *",
+            "**** ",
+            "*    ",
+            "*    ",
+            "*    "
+        };
+    }
+
+    // Method to generate pattern for letter 'S'
+    public static String[] getSPattern() {
+        return new String[]{
+            " ****",
+            "*    ",
+            "*    ",
+            " *** ",
+            "    *",
+            "    *",
+            "**** "
+        };
+    }
+
+    // Main method
+    public static void main(String[] args) {
+
+        String[] o = getOPattern();
+        String[] p = getPPattern();
+        String[] s = getSPattern();
+
+        // Print OOPS banner
+        for (int i = 0; i < o.length; i++) {
+            System.out.println(o[i] + "  " + o[i] + "  " + p[i] + "  " + s[i]);
+        }
+    }
 }
